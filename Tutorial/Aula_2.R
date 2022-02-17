aa9ebb0ed2e6cdc87a958496c71efce6c275facb
Gustavo Maciel <79233238+Gus-1003@users.noreply.github.com>
2022-02-17 00:40:17 -0300

    Create Aula_2.R

---
+++ Tutorial/Aula_2.R
@@ -0,0 +1,41 @@
+#Operadores Aritmetcos
+
+5 + 5 #soma
+9 - 4 #Subtração
+10 * 2 #Multiplicação
+14 / 2 #Divisão
+7 / 2 #Divisão
+4 ** 2 #Potencia
+4 ^ 2 #Potencia
+
+#Operadores de equivalencia 
+
+5 == 6 #Teste de equivalencia (igual)
+5 == 5 #Teste de equivalencia (igual)
+
+5 != 6 #Teste de diferença
+5 != 5 #Teste da diferença
+
+5 > 6 #Teste de equivalencia (maior)
+5 > 4 #Teste de equivalencia (maior)
+
+5 < 6 #Teste de equivalencia (menor)
+5 < 4 #Teste de equivalencia (menor)
+
+5 >= 4 #Teste de equivalencia (maior ou igual)
+5 >= 5 #Teste de equivalencia (maior ou igual)
+
+5 <= 4 #Teste de equivalencia (menor ou igual)
+5 <= 5 #Teste de equivalencia (menor ou igual)
+
+#Operadores Logicos
+
+#1. Operador de conjunção (&)
+# (tudo precisa ser verdadeiro para dar verdadeiro)
+5 > 4 & 7 == 7
+5 < 4 & 7 == 7
+5 == 5 & 7 < 6
+1 > 2 & 8 < 5
+
+#2. Operador de disjunção (|)
+#3. Operador de negação (! antes de uma das sentenças)
